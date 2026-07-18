95b4591cff3789b026e8f2241f3c2a8a390867ed
jdavison-dev <jackhdavison@icloud.com>
2026-07-18 18:00:29 +0900

    chore: move git tracking to root and back to true  monorepo structure

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware to parse incoming JSON stuff
+app.use(cors());
 app.use(express.json());
 
 // Import route Files
